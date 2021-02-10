753e615365a0fbb69e61ce5087f0c84e4a4468a1
Thiagodsm <thiagosm.004@gmail.com>
2021-02-10 15:39:19 -0300

    first commit

---
+++ sockets.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+
+#define MAX_REQ_LEN 1000
+#define FBLOCK_SIZE 4000
+
+#define COD_OK0_GET         "OK-0 method GET OK\n"
+#define COD_OK1_FILE         "OK-1 File open OK\n"
+#define COD_ERROR_0_METHOD  "ERROR-0 Method not supported\n"
+#define COD_ERROR_1_FILE "ERROR-1 File could not be open\n"
+
+#define REQ_GET "GET"
+
+struct req_t {
+    char method[128];
+    char filename[256];
+};
+typedef struct req_t req_t;
+
+void get_request(req_t * resquest, char *rstr){
+    bzero(resquest, sizeof(req_t));
+    sscanf(rstr, "%s %s", resquest->method, resquest->filename);
+}
+
+void send_file(int sockfd, req_t request){
+    int fd, nr;
+    unsigned char fbuff[FBLOCK_SIZE];
+    fd = open(request.filename, O_RDONLY, S_IRUSR);
+    if(fd == -1){
+        perror("open");
+        send(sockfd, COD_ERROR_1_FILE, strlen(COD_ERROR_1_FILE), 0);
+        return;
+    }
+    send(sockfd, COD_OK1_FILE, strlen(COD_OK1_FILE), 0);
+    do{
+        bzero(fbuff, FBLOCK_SIZE);
+        nr = read(fd, fbuff, FBLOCK_SIZE);
+        if (nr > 0){
+            send(sockfd, fbuff, nr, 0);
+        }
+    } while (nr > 0);
+    close(fd);
+    return;
+    
+}
+
+void proc_request( int sockfd, req_t request){
+    if (strcmp(request.method, REQ_GET) == 0){
+        send(sockfd, COD_OK0_GET, strlen(COD_OK0_GET), 0);
+        send_file(sockfd, request);
+    }
+    else{
+        send(sockfd, COD_ERROR_0_METHOD, strlen(COD_ERROR_0_METHOD), 0);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 2){
+        printf("Uso: %s <porta>\n", argv[0]);
+        return 0;
+    }
+    int sl; // descritor de arquivo
+    sl = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sl == -1){
+        perror("socket");
+        return 0;
+    }
+    struct sockaddr_in saddr; 
+    saddr.sin_port = htons(atoi(argv[1])); // o valor da porta esta em argv[1], e necessario converter para int, e para a ordem do byte correto
+    saddr.sin_family = AF_INET; // ipv4
+    saddr.sin_addr.s_addr = INADDR_ANY; // endereço IP vinculado a porta, faz o bind em todos os endereços IPs existentes na maq
+    if(bind(sl, (struct sockaddr *)&saddr, sizeof(struct sockaddr_in)) == -1){
+        perror("bind");
+        return 0;
+    }
+    // 1000 clientes podem estar aguardando conexão com o servidor
+    if (listen(sl, 1000) == -1){
+        perror("listen");
+        return 0;
+    }
+    
+    struct sockaddr_in caddr;
+    int addr_len;
+    int sc, nr; // socket cliente
+    char resquest[MAX_REQ_LEN];
+    req_t req;
+
+    while (1){
+        addr_len = sizeof(struct sockaddr_in);
+        sc = accept(sl, (struct sockaddr*)&caddr, (socklen_t *)&addr_len);
+        if(sc == -1){
+            perror("accept");
+            continue;
+        }
+
+        printf("Conectado com o cliente %s:%d\n", inet_ntoa(caddr.sin_addr), ntohs(caddr.sin_port));
+
+        bzero(resquest, MAX_REQ_LEN);
+        nr = recv(sc, resquest, MAX_REQ_LEN, 0);
+        if(nr > 0){
+            get_request(&req, resquest);
+            printf("method: %s\nfilename: %s\n", req.method, req.filename);
+            proc_request(sc, req);
+        }
+        //send
+        close(sc);
+    }
+    close(sl);
+    return 0;
+}
